326824a282a958308cce88ed42b0f060b1fd438f
Javarious Madison <lordxm.madison@gmail.com>
2022-06-16 13:01:44 -0400

    Added hello dolly print again having trouble pushing, not showing up in remoote repo

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -2,4 +2,9 @@ package ver1;
 
 public class HelloDolly {
 
+	public static void main() {
+		System.out.println("Hello Dolly from Exclipse");
+		
+		System.out.println("Hello Dolly from Eclipse Again");
+	}
 }
